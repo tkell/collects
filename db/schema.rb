9d11bcaec2c3bea1ad33a1fbe6187a5c2d9dec77
Thor Kell <thor@tide-pool.ca>
2023-11-20 19:06:58 -0500

    add garden model, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_235505) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_000546) do
   create_table "collections", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "gardens", force: :cascade do |t|
+    t.string "name"
+    t.integer "collection_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["collection_id"], name: "index_gardens_on_collection_id"
+  end
+
+  add_foreign_key "gardens", "collections"
 end
