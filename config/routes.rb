53c9fae37d435731e5763bc16e13e32841eba126
Thor Kell <thor@tide-pool.ca>
2023-11-20 19:10:11 -0500

    update collection with has_many, update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/collections", to: "collections#index"
+  get "/collections",     to: "collections#index"
   get "/collections/:id", to: "collections#show", as: 'collection'
-
+  get "/gardens:id",      to: "gardens#show"
 end
