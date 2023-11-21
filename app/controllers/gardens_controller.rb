8bd0551619ac801b4bb04083492a182c180d2733
Thor Kell <thor@tide-pool.ca>
2023-11-20 20:35:31 -0500

    Almost creating gardens correctly

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,5 +1,34 @@
 class GardensController < ApplicationController
+  def index
+    @gardens = Garden.all
+  end
+
   def show
     @garden = Garden.find(params[:id])
+  end
+
+  def new
+    @collection = Collection.find(params[:collection_id])
+    @garden = @collection.gardens.new
+  end
+
+  def create
+    @collection = Collection.find(params[:collection_id])
+    @garden = @collection.gardens.create(garden_params)
+
+    if @garden.save
+      ## OK, we're close, we need to fix this redirect, but nbd!
+
+      redirect_to @garden
+    else
+      puts(@garden.errors.full_messages)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name)
   end
 end
