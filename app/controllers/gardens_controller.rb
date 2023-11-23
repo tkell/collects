5c3f8bf1661b33b0bba5b65d17ce7c8225fd24d7
Thor Kell <thor@tide-pool.ca>
2023-11-22 22:25:41 -0500

    adding garden items via garden update

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -23,9 +23,25 @@ class GardensController < ApplicationController
     end
   end
 
+  def edit
+    @collection = Collection.find(params[:collection_id])
+    @garden = Garden.find(params[:id])
+  end
+
+  def update
+    @collection = Collection.find(params[:collection_id])
+    @garden = Garden.find(params[:id])
+
+    if @garden.update(garden_params)
+      redirect_to collection_garden_path(@collection, @garden)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def garden_params
-    params.require(:garden).permit(:name)
+    params.require(:garden).permit(:name, garden_items_attributes: [:id, :item_id, :_destroy])
   end
 end
--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,4 +2,6 @@ class Garden < ApplicationRecord
   belongs_to :collection
   has_many :garden_items
   has_many :items, through: :garden_items
+
+  accepts_nested_attributes_for :garden_items
 end
