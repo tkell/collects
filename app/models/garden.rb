f9c65166084646ba500f3aae78ba83520f3aaf9c
Thor Kell <thor@tide-pool.ca>
2023-11-30 18:24:34 -0500

    add and remove single garden items

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,5 +3,5 @@ class Garden < ApplicationRecord
   has_many :garden_items
   has_many :items, through: :garden_items
 
-  accepts_nested_attributes_for :garden_items
+  accepts_nested_attributes_for :garden_items, allow_destroy: true, reject_if: :all_blank
 end
