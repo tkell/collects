e8ef87b7cbb2d192a2a96427dd7695a8ebed7bb0
Thor Kell <thor@tide-pool.ca>
2023-12-07 21:14:03 -0500

    aliasing subitem to track, close enough

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :collection
   has_many :garden_items
-  has_many :subitems
+  has_many :tracks, foreign_key: 'item_id', class_name: 'Subitem'
 
   serialize :colors, coder: JSON, type: Array
 
   def as_json(options={})
-    super(include: :subitems)
+    super(include: :tracks)
   end
 end
