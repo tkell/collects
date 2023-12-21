5d294b5eef0b21231e1075a144e591ed49786385
Thor Kell <thor@tide-pool.ca>
2023-12-20 20:37:31 -0500

    folder and filter, refactor to pass conditionals

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,14 +6,23 @@ class CollectionsController < ApplicationController
   def show
     if params.has_key?(:serve_json)
       p = tessellates_params
-      filter_string = "%" + Release.sanitize_sql_like(p[:filter_string]) + "%"
-      render json: Collection.find(params[:id])
-        .releases
-        .where("artist LIKE :search_string OR title LIKE :search_string OR label LIKE :search_string", {search_string: filter_string})
-        #.where(folder: p[:folder])
+      data = Collection.find(params[:id]).releases
+
+      if p[:folder]
+        data = data.where(folder: p[:folder])
+      end
+      if p[:filter_string]
+        filter_string = "%" + Release.sanitize_sql_like(p[:filter_string]) + "%"
+        data = data
+          .where("artist LIKE :search_string OR title LIKE :search_string OR label LIKE :search_string", {search_string: filter_string})
+      end
+
+      data = data
         .limit(p[:limit])
         .offset(p[:offset])
         .includes(:tracks)
+
+      render json: data
     else
       @collection = Collection.includes(releases: :tracks).find(params[:id])
     end
@@ -23,6 +32,8 @@ class CollectionsController < ApplicationController
   private
 
   def tessellates_params
-    params.permit(:id, :serve_json, :limit, :offset, :filter_string).with_defaults(limit: 100, offset: 0, filter_string: "")
+    params
+      .permit(:id, :serve_json, :limit, :offset, :filter_string, :folder)
+      .with_defaults(limit: 100, offset: 0, filter_string: nil, folder: nil)
   end
 end
