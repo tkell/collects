d78a7a3211a89ae96be5c97ebd7bd3d78bc2ba95
Thor Kell <thor@tide-pool.ca>
2024-01-17 20:59:14 -0500

    remove SSL force

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # Disabled for AWS Elastic Beanstalk Deploy
+  # config.force_ssl = true
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
