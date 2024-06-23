ff1074b906ec2e76e1ecf76d40c72674fdd67613
Thor Kell <thor@tide-pool.ca>
2024-06-23 14:01:52 -0400

    correct CORS settings in rails

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://tide-pool.ca', 'https://www.tide-pool.ca'
+    origins 'localhost:3000', 'tide-pool.ca', 'www.tide-pool.ca'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
