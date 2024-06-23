3e6b7833be9d1a0089201e410724fb7c876ca831
Thor Kell <thor@tide-pool.ca>
2024-06-23 12:14:44 -0400

    users must have unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :gardens, through: :collections
   has_many :releases, through: :collections
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :username, presence: true
 end
