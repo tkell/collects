d22523aa98821cf6d92bebaa5c7107d64bc32566
Thor Kell <thor@tide-pool.ca>
2024-07-20 11:14:03 -0400

    increase cookie timeout

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       token = JWT.encode({user_id: user.id}, Rails.application.secrets.secret_key_base)
-      expires_at = Time.now + 1.day
+      expires_at = Time.now + 30.days
       cookies.encrypted[:jwt] = {
         value: token,
         expires: expires_at,
