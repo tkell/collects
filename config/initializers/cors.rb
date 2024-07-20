78fbaa3b60061a5fa2f9d12f957740adc03bcf5e
Thor Kell <thor@tide-pool.ca>
2024-07-20 11:13:12 -0400

    add collects hostname to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost', 'tide-pool.ca', 'www.tide-pool.ca'
+    origins 'localhost', 'tide-pool.ca', 'www.tide-pool.ca', 'collects.tide-pool.ca'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
