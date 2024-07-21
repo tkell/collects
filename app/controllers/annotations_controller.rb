a48771da33cca9acba4edcc34d6965b32806a87a
Thor Kell <thor@tide-pool.ca>
2024-07-20 21:33:32 -0400

    display annotations by type, downcase all but freeform

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -5,6 +5,15 @@ class AnnotationsController < ApplicationController
   def index
     @release = Release.where(id: params['release_id']).includes(:tracks).first
     @annotations = Annotation.where(release_id: @release.id)
+
+    @annotations_by_type = {}
+    @annotations.each do |annotation|
+      if @annotations_by_type[annotation.annotation_type].nil?
+        @annotations_by_type[annotation.annotation_type] = []
+      end
+      @annotations_by_type[annotation.annotation_type].push(annotation)
+    end
+    puts @annotations_by_type
   end
 
   def new
@@ -14,8 +23,10 @@ class AnnotationsController < ApplicationController
   def create
     @release_id = params['release_id']
     @annotation_type = params['annotation_type'].to_i
-    @body = params['body']
     user_id = @current_user_id
+    if @annotation_type != 3
+      @body = params['body'].downcase
+    end
     @annotation = Annotation.new(release_id: @release_id, annotation_type: @annotation_type, body: @body, user_id: user_id)
 
     if @annotation.save
