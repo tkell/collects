d74f5f30672621e9dd428fe20cc448f6a74c8223
Thor Kell <thor@tide-pool.ca>
2025-11-23 11:15:51 -0500

    add grouping

--- app/controllers/playbacks_controller.rb
+++ app/controllers/playbacks_controller.rb
@@ -5,6 +5,7 @@ class PlaybacksController < ApplicationController
     p = playbacks_read_params
     start_date = Date.parse(p[:start_date])
     end_date = Date.parse(p[:end_date])
+    grouping = p[:g]
     all_playbacks = Playback
       .joins(:release)
       .where("playbacks.created_at >= ?", start_date).where("playbacks.created_at <= ?", end_date)
@@ -19,7 +20,26 @@ class PlaybacksController < ApplicationController
     end
     sorted_counts = counts.sort_by { |release_id, count| count }.reverse!
 
-    render json: {playbacks: all_playbacks, counts: sorted_counts, releases: releases}, status: :ok
+
+    groups = Hash.new()
+    i = start_date
+    while i < end_date do
+        key = get_key(i, grouping)
+        groups[key] = Hash.new(0)
+        i = i + 1.send(grouping) # ruby magic!
+    end
+
+    puts(groups)
+    all_playbacks.each do |p|
+      group_key = get_key(p.created_at, grouping)
+      groups[group_key][p.release_id] += 1
+    end
+    groups.keys.each do |k|
+      groups[k] = groups[k].sort_by { |release_id, count| count }.reverse!
+    end
+
+
+    render json: {playbacks: all_playbacks, counts: sorted_counts, releases: releases, groups: groups}, status: :ok
   end
 
   def new
@@ -48,11 +68,22 @@ class PlaybacksController < ApplicationController
   end
 
   def playbacks_read_params
-    today = Date.today + 1.day
-    today_str = today.to_s
-    epoch_str = "1970-1-1"
+    end_date = Date.today + 1.day
+    end_date_str= end_date.to_s
+    year = Date.today.year
+    start_of_year_str = Date.new(year).to_s
     params
-      .permit(:start_date, :end_date, "playback")
-      .with_defaults(start_date: epoch_str, end_date: today_str)
+      .permit(:start_date, :end_date, :g, "playback")
+      .with_defaults(start_date: start_of_year_str, end_date: end_date_str, g: "month")
+  end
+
+  def get_key(date, grouping)
+    if grouping == "year"
+      return Date.new(date.year)
+    elsif grouping == "month"
+      return Date.new(date.year, date.month)
+    elsif grouping == "day"
+      return Date.new(date.year, date.month, date.day)
+    end
   end
 end
