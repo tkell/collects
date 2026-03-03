ee772944a5e3a331c809b76ac7de1bd8ae621723
Thor Kell <thor@tide-pool.ca>
2026-03-02 21:02:51 -0500

    more emoji and texts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  RESPONSE_EMOJIS = ['🩵','💓', '🎵', '🛸', '🔊'].freeze
+  RESPONSE_EMOJIS = ['🩵','💓', '💚', '🧡', '💙', '🎵', '🛸', '🔊', '✨', '💫', '💥', '🚀'].freeze
   TEXT_ARTS = [
     '>§»',
     '¤⁀℮⁀⎀',
     '⁁⁂Ɲ‱ܐ',
+    '∰ 〜✎ẜ ͦ ͦ',
+    '△↴∛⋱⌧',
+    '℄∑╠▞▐',
+    'ㄲጦ 🝃 𝄫',
+    '૧Ⓠ◓☀✪',
   ].freeze
 
   default from: ENV.fetch("MAILER_FROM", "noreply@tessellates.space")
