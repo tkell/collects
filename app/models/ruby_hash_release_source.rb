f1d16d20f8eb0cd359db73b59e38c98d5eca74a1
Thor Kell <thor@tide-pool.ca>
2026-03-10 20:52:06 -0400

    use image_path from input

--- app/models/ruby_hash_release_source.rb
+++ app/models/ruby_hash_release_source.rb
@@ -7,19 +7,12 @@ class RubyHashReleaseSource < ReleaseSource
       release_data = release_data.dup
       release_data["release_year"] = release_data["year"]
       release_data["external_id"] = release_data["id"].to_s
-      release_data["image_path"] = get_image_path(release_data)
+      release_data["image_path"] = release_data["image_url"]
       release_data.delete("id")
       release_data.delete("year")
       all_releases << release_data
     end
 
     load_all_releases(all_releases, current_releases, overwrite_strategy)
-  end
-
-  private
-
-  def get_image_path(release_data)
-    external_id = release_data["id"].to_s
-    "https://tide-pool.ca/tessellates/#{collection.name.downcase}/images/#{external_id}.jpg"
   end
 end
