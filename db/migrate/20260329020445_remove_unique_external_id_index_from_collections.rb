23c725b334c4d3f9ef6b4bea578745eefeb79c68
Thor Kell <thor@tide-pool.ca>
2026-04-07 21:58:24 -0400

    bad index fix, but works for now

--- db/migrate/20260329020445_remove_unique_external_id_index_from_collections.rb
+++ db/migrate/20260329020445_remove_unique_external_id_index_from_collections.rb
@@ -1,5 +1,7 @@
 class RemoveUniqueExternalIdIndexFromCollections < ActiveRecord::Migration[7.1]
   def change
-    remove_index :collections, name: 'idx_16450_index_releases_on_external_id'
+    ## different names on production and local, yikes?
+    # remove_index :collections, name: 'idx_16454_index_releases_on_external_id' # prod
+    # remove_index :collections, name: 'idx_16450_index_releases_on_external_id' # local
   end
 end
