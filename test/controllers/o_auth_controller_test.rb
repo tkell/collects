f4e998f7788b748e91131e3220206fd7185906fa
Thor Kell <thor@tide-pool.ca>
2026-08-11 20:35:06 -0400

    remove old bad tests

--- test/controllers/o_auth_controller_test.rb
+++ test/controllers/o_auth_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class OAuthControllerTest < ActionDispatch::IntegrationTest
-  test "authorize returns unsupported provider" do
-    get oauth_authorize_url(provider: 'unknown'), headers: authenticated_headers
-    assert_response :unprocessable_entity
-  end
-
-  test "callback returns unsupported provider" do
-    get oauth_callback_url(provider: 'unknown'), headers: authenticated_headers
-    assert_response :unprocessable_entity
-  end
 end
