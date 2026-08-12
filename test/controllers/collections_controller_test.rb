27f3574b9513a62d88b5faad130215e2998d0cec
Thor Kell <thor@tide-pool.ca>
2026-08-11 20:42:28 -0400

    first basic controller test

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class CollectionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index returns the current user's collections" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    assert_response :success
+
+    get collections_url
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 1, body.length
+    assert_equal collections(:one).name, body.first["name"]
+  end
 end
