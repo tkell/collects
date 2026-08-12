f226558c1a9ff992c591517303db447b0570d33f
Thor Kell <thor@tide-pool.ca>
2026-08-11 20:45:08 -0400

    test for filters, etc

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -13,4 +13,17 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 1, body.length
     assert_equal collections(:one).name, body.first["name"]
   end
+
+  test "show filters releases by release_year" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    assert_response :success
+
+    get collection_url(collections(:one).name), params: {release_year: "1999"}
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 1, body.length
+    assert_equal releases(:one).title, body.first["title"]
+  end
 end
