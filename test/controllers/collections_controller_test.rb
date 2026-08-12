53a54f87af756f60addd1c4603472b56c37b4ee7
Thor Kell <thor@tide-pool.ca>
2026-08-11 20:57:03 -0400

    more main controller tests

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -26,4 +26,69 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 1, body.length
     assert_equal releases(:one).title, body.first["title"]
   end
+
+  test "show filters releases by purchase_date" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    assert_response :success
+
+    get collection_url(collections(:one).name), params: {purchase_date: "2015"}
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 1, body.length
+    assert_equal releases(:three).title, body.first["title"]
+  end
+
+  test "show filters releases by purchase_date range" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    assert_response :success
+
+    get collection_url(collections(:one).name), params: {purchase_date: "2014-2016"}
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 1, body.length
+    assert_equal releases(:three).title, body.first["title"]
+  end
+
+  test "show filters releases by artist" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    assert_response :success
+
+    get collection_url(collections(:one).name), params: {filter: "Siamese"}
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 1, body.length
+    assert_equal releases(:one).title, body.first["title"]
+  end
+
+  test "show filters releases by title" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    assert_response :success
+
+    get collection_url(collections(:one).name), params: {filter: "Bengal"}
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 1, body.length
+    assert_equal releases(:two).title, body.first["title"]
+  end
+
+  test "show filters releases by label" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    assert_response :success
+
+    get collection_url(collections(:one).name), params: {filter: "Burmese"}
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal 1, body.length
+    assert_equal releases(:three).title, body.first["title"]
+  end
 end
