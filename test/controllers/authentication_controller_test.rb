74c2e59757d1ecc788b06892a8964487291343c5
Thor Kell <thor@tide-pool.ca>
2026-08-14 21:13:48 -0400

    auth tests

--- test/controllers/authentication_controller_test.rb
+++ test/controllers/authentication_controller_test.rb
@@ -11,4 +11,16 @@ class AuthenticationControllerTest < ActionDispatch::IntegrationTest
     assert_equal "Logged in", body["message"]
     assert_equal user.id, body["user_id"]
   end
+
+  test "logout clears session and cookie" do
+    user = users(:one)
+    post login_url, params: {email: user.email, password: "password123"}
+    delete logout_url
+
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal "Logged out", body["message"]
+    assert_nil session[:user_id]
+    assert_equal cookies[:jwt], ""
+  end
 end
